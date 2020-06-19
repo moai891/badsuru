dd84d058c5a63852f95c6ec69c741b8758e116d0
moai891 <ktdkmk2@gmail.com>
2020-06-19 22:11:34 +0900

    create_user_function
    ユーザーのウィザード形式の新規登録・ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one  :profile
   has_many :groups, through: :members, dependent: :destroy
   has_many :practices, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :joinings, dependent: :destroy
-  validates :nickname, :old, :job_id, :experience, :practice_time, presence: true
-  validates :phonenumber, null: false
+  validates :nickname, presence: true
 end
