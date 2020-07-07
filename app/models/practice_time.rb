75060876d2d31ecba3a19ffddea732cf4fa8dd54
moai891 <ktdkmk2@gmail.com>
2020-07-07 21:36:58 +0900

    change model_name
    エラー修正のためモデル名を変更

--- app/models/practice_time.rb
+++ app/models/practice_time.rb
@@ -1,4 +1,4 @@
-class Practice_time < ActiveHash::Base
+class PracticeTime < ActiveHash::Base
   self.data = [
   {id: 1, name: '週4回以上'}, {id: 2, name: '週3〜4回'}, {id: 3, name: '週2〜3回'},{id: 4, name: '週1〜2回'},{id: 5, name: '月数回'},{id: 6, name: '年数回'},{id: 7, name: 'その他'}
   ]
